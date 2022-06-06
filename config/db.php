5dd8bc7a73d6d5826466c7c249a7809e593be64f
erelmuan <eliasrelmuan@gmail.com>
2022-06-06 10:15:31 -0300

    Se modifica git ignore y cambio en la conf de la base

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
   'class' => 'yii\db\Connection',
   'dsn' => 'pgsql:host=localhost;dbname=hazpatologia',
-  'username' => 'elias',
+  'username' => 'postgres',
   'password' => '123',
   'charset' => 'utf8',
 
