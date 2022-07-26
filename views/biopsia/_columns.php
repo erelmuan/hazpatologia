2a9a416de44fb4ea31fd9bea76bf4b08e46e1240
elias <eliasrelmuan@gmail.com>
2022-07-26 08:43:31 -0300

    correccion de columna- se agrega numero de version
    Se corrige columnas de biopsias , se incorpora nueva version.

--- views/biopsia/_columns.php
+++ views/biopsia/_columns.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 return [
         [
         'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'biopsia.id',
+        'attribute'=>'id',
     ],
     [
         'attribute' => 'protocolo',
